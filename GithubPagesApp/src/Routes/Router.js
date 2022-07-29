75facc6dd8e65344e8347e43b75169148831b1a5
dylanbrine <dylan.brine@milkmoovement.com>
2022-07-28 22:05:34 -0300

    updated router

--- GithubPagesApp/src/Routes/Router.js
+++ GithubPagesApp/src/Routes/Router.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const Router = () => {
   return (
-    <Switch>
-      <Route path='/react-app-for-gh-pages/'>
-        <p>this is the home screen</p>
-      </Route>
-      <Route path='/test/'>
-        <p>test</p>
-      </Route>
-    </Switch>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/react-app-for-gh-pages/'
+          element={<p>this is the home screen</p>}></Route>
+        <Route path='/test/' element={<p>test</p>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
